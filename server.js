dde30a28126a8f9ceaff101a335023d3e6155703
Ali <96638533+freakdeco@users.noreply.github.com>
2024-04-25 05:10:51 +0000

    Video 15 - Custom error handler

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
 
 import userRoutes from "./routes/userRoutes";
 
@@ -14,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
